133bfd83bedded231e28d64f10d3b8b0a70d478c
Jake Robins <me@jakerobins.com>
2023-02-23 08:22:35 -0600

    add fields to party messages

--- schemas/eventPartyMessages.js
+++ schemas/eventPartyMessages.js
@@ -8,5 +8,16 @@ export default {
       type: "string",
       title: "Message",
     },
+    {
+      name: "force",
+      type: "boolean",
+      title: "Force",
+    },
+    {
+      name: "timestamp",
+      type: "number",
+      title: "Time",
+      validation: (Rule) => Rule.max(60).min(0),
+    },
   ],
 };
